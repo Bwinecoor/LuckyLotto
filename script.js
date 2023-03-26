30ebf0c376af81714a06d16cefc3393f48b3b326
Bwinecoor <123968562+Bwinecoor@users.noreply.github.com>
2023-03-26 02:04:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+function clearName() {
+    if (document.getElementById('nameField').value == 'First and Last Name'){
+    document.getElementById('nameField').value = '';
+}
+}
+function resetName() {
+    if (document.getElementById('nameField').value == ''){
+    document.getElementById('nameField').value = 'First and Last Name';
+}
+}
+function clearPhone() {
+    if (document.getElementById('phoneField').value == '123-456-7890'){
+    document.getElementById('phoneField').value = '';
+}
+}
+function resetPhone() {
+    if (document.getElementById('phoneField').value == ''){
+    document.getElementById('phoneField').value = '123-456-7890';
+}
+}
+function clearEmail() {
+    if (document.getElementById('emailField').value == 'myemail@gmail.com'){
+    document.getElementById('emailField').value = '';
+}
+}
+function resetEmail() {
+    if (document.getElementById('emailField').value == ''){
+    document.getElementById('emailField').value = 'myemail@gmail.com';
+}
+}
+// --------------------------------------------------------------------------------------
+function clearCardNumber() {
+    if (document.getElementById('cardNumField').value == '1234-5678-9101-1121'){
+    document.getElementById('cardNumField').value = '';
+}
+}
+function resetCardNumber() {
+    if (document.getElementById('cardNumField').value == ''){
+    document.getElementById('cardNumField').value = '1234-5678-9101-1121';
+}
+}
+// --------------------------------------------------------------------------------------
+function clearExpDate() {
+    if (document.getElementById('expDateField').value == '12/31'){
+    document.getElementById('expDateField').value = '';
+}
+}
+function resetExpDate() {
+    if (document.getElementById('expDateField').value == ''){
+    document.getElementById('expDateField').value = '12/31';
+}
+}
+// --------------------------------------------------------------------------------------
+
+function clearCVV() {
+    if (document.getElementById('CVVField').value == '123'){
+    document.getElementById('CVVField').value = '';
+}
+}
+function resetCVV() {
+    if (document.getElementById('CVVField').value == ''){
+    document.getElementById('CVVField').value = '123';
+}
+}
